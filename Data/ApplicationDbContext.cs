2277c0c15ccb4d8dc4bf7b9f2b741e925435ddae
Дмитрий <rabolan@mail.ru>
2019-05-23 18:02:34 +0300

    Добавьте файлы проекта.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TheBestApp.Models;
+
+namespace TheBestApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<TheBestApp.Models.Person> Person { get; set; }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBestApp.Models
+{
+    /// <summary>
+    /// Обьектная модель человека
+    /// </summary>
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }   
+
+        public int Age { get; set; }
+
+    }
+}
